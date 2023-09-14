7d3d484ecf8ebf5c614777a45538d5e2feda4749
Mohd Umar <khanumar@gmail.com>
2023-09-14 10:32:19 +0530

    speech feature added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,27 @@
 import React from 'react'
 
-export default function Main({currentNote,updateNote}) {
+export default function Main({ currentNote, updateNote }) {
 
-//    const [text,setText] = React.useState("")
+    //    const [text,setText] = React.useState("")
 
-   function handleChange(event){
-      const txt = event.target.value      
-       updateNote(txt)
-   }
-  
+    function handleChange(event) {
+        const txt = event.target.value
+        updateNote(txt)
+    }
+
+    function handleSound() {
+        let msg = currentNote.body
+        let utterance = new SpeechSynthesisUtterance(msg);
+        speechSynthesis.speak(utterance)
+    }
+
     // console.log(currentNote)
     return (
         <div className='main-area'>
             <h2>Notes</h2>
             <textarea className="textNote" name="text" cols="100" rows="10" onChange={handleChange} value={currentNote.body}></textarea>
-                
+            <br/>
+            <img src="sound.png" alt="" className="btn btn-primary sound" onClick={handleSound}></img>
         </div>
     )
 }
